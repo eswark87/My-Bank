f20a33e1b21bf654f2cc3f6f704caefde4650715
eswark87 <saieswarabd3035@gmail.com>
2024-05-22 16:11:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Router from "./config/Router";
+
+function App() {
+
+    return <>
+        <Router/>
+    </>;
+}
+
+export default App;
---
+++ src/components/user/HomePage.js
@@ -0,0 +1,16 @@
+import styles from "./HomePage.module.css";
+import SpaceDiv from "../UI/SpaceDiv";
+
+function HomePage() {
+    return <div className={`${styles.mainDiv} flex-fill`}>
+        <SpaceDiv height={5}/>
+        <h1 className={styles.title}>Welcome to MyBank</h1>
+        <p className={styles.subtitle}> Welcome to our bank, where we are committed to providing exceptional financial services to individuals,
+            businesses,
+            and communities. Our bank is built on a foundation of trust and reliability and with a team of
+            professionals.
+        </p>
+    </div>;
+}
+
+export default HomePage;
